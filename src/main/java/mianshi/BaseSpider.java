6882b205065efdc8629be83dd6a11f2e548621da
tudouzhao <1968501583@qq.com>
2017-03-14 13:52:35 +0800

    稍微修改一下自己的基类方法

--- src/main/java/mianshi/BaseSpider.java
+++ src/main/java/mianshi/BaseSpider.java
@@ -3,6 +3,11 @@
  */
 package mianshi;
 
+import java.util.Scanner;
+
+import org.apache.commons.io.*;
+import org.apache.commons.lang.StringUtils;
+
 /**
  * 下午1:36:06
  * 
@@ -14,14 +19,23 @@ public abstract class BaseSpider {
 	// 基础解法
 
 	public void count() {
-		System.out.println("基础方法测试:");
-		baseCount();
 
-		System.out.println("较好方法测试:");
-		betterCount();
+		System.out.println("选择要测试的方法:1:base;2:better;3:best");
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
 
-		System.out.println("最好方法测试:");
-		bestCount();
+		if (StringUtils.isEmpty(line) || line.equals("1")) {
+			System.out.println("基础方法测试:");
+			baseCount();
+		}
+		if (line.equals("2")) {
+			System.out.println("较好方法测试:");
+			betterCount();
+		}
+		if (line.equals("3")) {
+			System.out.println("最好方法测试:");
+			bestCount();
+		}
 	}
 
 	public abstract void baseCount();
