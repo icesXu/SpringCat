5cd95420fe4161b84ce3e7e42c63d8c8d5b249bb
tudouzhao <1968501583@qq.com>
2017-03-20 23:43:46 +0800

    修改一下存放的位置

---
+++ src/main/java/annotation/fruit/FruitInfoUtil.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package annotation.fruit;
+
+import java.lang.reflect.Field;
+
+/**
+ * 下午1:45:55
+ * 
+ * @author yuzhao.yang
+ *
+ */
+public class FruitInfoUtil {
+	public static void getFruitInfo(Class<?> clazz) {
+		String strFruitName = "水果名称";
+		String strFruitColor = "水果颜色";
+		String strFruitProvider = "水果供应商";
+
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			// 如果出现了该注解
+			if (field.isAnnotationPresent(FruitName.class)) {
+				FruitName fruitName = (FruitName) field
+						.getAnnotation(FruitName.class);
+				strFruitName = strFruitName + fruitName.value();
+				System.out.println(strFruitName);
+			}
+			if (field.isAnnotationPresent(FruitColor.class)) {
+				FruitColor fruitColor = (FruitColor) field
+						.getAnnotation(FruitColor.class);
+				strFruitColor = strFruitColor
+						+ fruitColor.fruitColor().toString();
+				System.out.println(strFruitColor);
+			}
+			if (field.isAnnotationPresent(FruitProvider.class)) {
+				FruitProvider fruitProvider = (FruitProvider) field
+						.getAnnotation(FruitProvider.class);
+				strFruitProvider = " 供应商编号：" + fruitProvider.id() + " 供应商名称："
+						+ fruitProvider.name() + " 供应商地址："
+						+ fruitProvider.address();
+				System.out.println(strFruitProvider);
+			}
+		}
+	}
+}
---
+++ src/main/java/annotation/fruit/FruitRun.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package annotation.fruit;
+
+
+/**
+ * 下午1:56:13
+ * 
+ * @author yuzhao.yang
+ *
+ */
+public class FruitRun {
+	public static void main(String[] args) {
+		FruitInfoUtil.getFruitInfo(Apple.class);
+	}
+
+}
