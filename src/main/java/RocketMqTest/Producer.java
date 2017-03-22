7ad38b9fbce0bac2a5be985ac4819d43605cd045
tudouzhao <1968501583@qq.com>
2017-03-22 13:18:24 +0800

    简单的rocketMq测试

---
+++ src/main/java/RocketMqTest/Producer.java
@@ -0,0 +1,44 @@
+package RocketMqTest;
+
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+
+public class Producer {
+
+	public static void main(String[] args) {
+		DefaultMQProducer producer = new DefaultMQProducer("Producer");
+		// nameserver服务,多个以;分开
+		producer.setNamesrvAddr("192.168.99.234:9876");
+		System.out.println(producer.getClientIP());
+		try {
+			producer.start();
+			producer.setVipChannelEnabled(false);
+
+			Message msg = new Message("PushTopic", "push", "1",
+					"Just for test.".getBytes());
+			SendResult result = producer.send(msg);
+			System.out.println("id:" + result.getMsgId() + " result:"
+					+ result.getSendStatus());
+			;
+
+			msg = new Message("PushTopic", "push", "2",
+					"Just for test.".getBytes());
+			result = producer.send(msg);
+			System.out.println("id:" + result.getMsgId() + " result:"
+					+ result.getSendStatus());
+			;
+
+			msg = new Message("PullTopic", "pull", "1",
+					"Just for test.".getBytes());
+			result = producer.send(msg);
+			System.out.println("id:" + result.getMsgId() + " result:"
+					+ result.getSendStatus());
+			;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			producer.shutdown();
+		}
+	}
+}
