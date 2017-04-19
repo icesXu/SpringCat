3286d7230e58861df1ea16c09add61fbee60c653
1968501583@qq.com <1968501593@qq.com>
2017-04-19 15:38:26 +0800

    简单的修改下。

---
+++ src/main/java/callback/another/Callback.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package callback.another;
+
+/**
+ * @author yuzhao.yang
+ * @description:
+ * @time:2017年4月19日 下午3:36:34
+ */
+public interface Callback {
+	void tellAnswer(int answer);
+}
---
+++ src/main/java/callback/another/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/** @author yuzhao.yang
+ * @description:
+ * @time:2017年4月19日 下午3:36:10
+ */
+package callback.another;
